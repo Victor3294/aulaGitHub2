9716e92db4da47602c00c62c86f1b1799fe10aaf
Victor <vblum26@gmail.com>
2023-08-17 17:07:10 -0300

    Primeira versão aula git2

---
+++ src/testeGit/LerNotas01.java
@@ -0,0 +1,22 @@
+package testeGit;
+
+
+import java.util.Scanner;
+
+public class LerNotas01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double[] notas = new double[5];
+		double soma = 0;
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Digite a nota do aluno " + (i+1));
+			notas[i] = input.nextDouble();
+			soma =+ notas[i];
+		}
+		System.out.println("A soma das notas dos alunos é " + soma);
+		input.close();
+
+	}
+
+}
