3320d082bcececf70c0016350da97b2b9eb49d2a
Victor <vblum26@gmail.com>
2023-08-21 14:29:47 -0300

    Contador de notas menores que 6

--- src/testeGit/LerNotas01.java
+++ src/testeGit/LerNotas01.java
@@ -10,15 +10,21 @@ public class LerNotas01 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		List<Double> lista = new ArrayList<>();
+		int contador = 0;
 		double soma = 0;
 		for(int i = 0; i < 5; i++) {
 			System.out.println("Digite a nota do aluno " + (i+1));
 			lista.add(input.nextDouble());
-			soma =+ lista.get(i);
+			double notas = lista.get(i);
+			if(notas < 6) {
+				contador++;
+			}
+			soma += notas;
 		}
 		System.out.println("A soma das notas dos alunos é " + soma);
 		double media = soma/lista.size();
 		System.out.println("A media das notas informadas é: " + media);
+		System.out.println("A quantidade de notas menores que 6 foi: " + contador);
 		input.close();
 
 	}
