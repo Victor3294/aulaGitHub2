f8ee2f49eb6b7027a0c967976109f6be599dec29
Victor <vblum26@gmail.com>
2023-08-21 14:40:04 -0300

    Função que verifica a maior e a menor nota digitada.

--- src/testeGit/LerNotas01.java
+++ src/testeGit/LerNotas01.java
@@ -12,6 +12,7 @@ public class LerNotas01 {
 		List<Double> lista = new ArrayList<>();
 		int contador = 0;
 		double soma = 0;
+		double menor = 0, maior = 0;
 		for(int i = 0; i < 5; i++) {
 			System.out.println("Digite a nota do aluno " + (i+1));
 			lista.add(input.nextDouble());
@@ -19,12 +20,22 @@ public class LerNotas01 {
 			if(notas < 6) {
 				contador++;
 			}
+			if(i == 0) {
+				menor = maior = notas;
+			}
+			else if(maior < notas) {
+				maior = notas;
+			}
+			if(menor > notas ) {
+				menor = notas;
+			}
 			soma += notas;
 		}
 		System.out.println("A soma das notas dos alunos é " + soma);
 		double media = soma/lista.size();
 		System.out.println("A media das notas informadas é: " + media);
 		System.out.println("A quantidade de notas menores que 6 foi: " + contador);
+		System.out.println("A maior nota é " + maior + " e a menor nota é: " + menor);
 		input.close();
 
 	}
